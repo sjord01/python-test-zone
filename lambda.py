547af503fb1156e466e631d94818dce67d15df5c
SJ Ordonez <samsonordonez.ce@gmail.com>
2024-01-02 15:12:08 -0800

    added lambda file, looking into the lambda functionality in python

---
+++ lambda.py
@@ -0,0 +1,19 @@
+people = [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Cho", "house": "Ravenclaw"},
+    {"name": "Draco", "house": "Slytherin"}
+]
+
+people.sort(key=lambda person: person["name"])
+#lambda (a single-line function) is similar to the function below
+
+'''
+def person(person):
+    return person["name"]
+'''
+
+print(people)
+
+""" Output:
+[{'name': 'Cho', 'house': 'Ravenclaw'}, {'name': 'Draco', 'house': 'Slytherin'}, {'name': 'Harry', 'house': 'Gryffindor'}]
+"""
